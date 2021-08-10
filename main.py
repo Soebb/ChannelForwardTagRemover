53e379b992931073bdce37b4d35e76fb51f8e4a3
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-10 12:49:09 +0430

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def start(bot, update):
 async def link_handler(c, m):
     headers = {
         "User-agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
-    URL = f'{m.text}'
+    URL = f'https://worldsubtitle.site/movies/{m.text}'
     r = scraper.get(URL, headers=headers)
     content = await URL.read()
     soup = BeautifulSoup(content, 'html.parser')
