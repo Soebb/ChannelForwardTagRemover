ca39de8c15ff216a6757b2f43a061a189c0818a6
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-10 16:23:56 +0430

    Update main.py

--- main.py
+++ main.py
@@ -41,22 +41,20 @@ async def start(bot, update):
 @bot.on_message(filters.regex(r'https?://[^\s]+') & filters.private)
 async def link_handler(c, m):
     headers = {
-        "User-agent": 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36'}
+        "User-agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
     URL = f'{m.text}'
-    try:
-        page = requests.get(URL, headers=headers)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        links = soup.findAll('a')
-        li = soup.findAll('p')
-        print(links)
-        print(li)
-        for link in links:
-            link_url = link["href"]
-            print(link_url)
-            await m.reply(f"{link_url}")
-    except Exception as e:
-        print(e)
-
+    r = scraper.get(URL, headers=headers)
+    content = await URL.read()
+    soup = BeautifulSoup(content, 'html.parser')
+    links = soup.findAll('a')
+    li = soup.findAll('p')
+    print(links)
+    print(li)
+    for link in links:
+        result = link['href']
+        print(f"{result}")
+        await m.reply(f"{result}")
+            
     
 
 
