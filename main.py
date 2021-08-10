112399e50cd71d22f104adbbbc8901b7b4222a0f
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-10 16:37:32 +0430

    Update main.py

--- main.py
+++ main.py
@@ -46,8 +46,8 @@ async def link_handler(c, m):
         "User-agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
     URL = f'{m.text}'
     r = scraper.get(URL, headers=headers)
-    content = await URL.read()
-    soup = BeautifulSoup(content, 'html.parser')
+    print(r.text)
+    soup = BeautifulSoup(r.content, 'html.parser')
     links = soup.findAll('a')
     li = soup.findAll('p')
     print(links)
