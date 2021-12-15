ebf0c2f6e117d398958f1f542c96590b28e8ab2a
Soebb <87156166+Soebb@users.noreply.github.com>
2021-12-15 20:15:53 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os, json
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import requests  
-
+import urllib.parse
 
 
 bot = Client(
@@ -14,10 +14,10 @@ bot = Client(
 
 @bot.on_message(filters.text)
 async def startt(bot, m):
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    #headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     url = 'https://poolam.ir/invoice/request'
-    params = {"api_key":"dd82f80e2c0746c16a975884a73b73b5", "amount":10000, "return_url":"http://tarafdari.com/node/2043656"}
-    r = requests.post(url, data=params, headers=headers)
+    params = {"api_key":"dd82f80e2c0746c16a975884a73b73b5", "amount":10000, "return_url":urllib.parse.quote("http://tarafdari.com/node/2043656")}
+    r = requests.post(url, data=params)
     print(r.json())
 
 
