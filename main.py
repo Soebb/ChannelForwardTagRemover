e05d7a482be6aef47e5413d3f4c58e782b0af137
Soebb <87156166+Soebb@users.noreply.github.com>
2021-12-16 17:38:41 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 from pydub import AudioSegment
+import os, json
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import requests  
+import urllib.parse
+
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+API_ID = os.environ.get('API_ID')
+API_HASH = os.environ.get('API_HASH')
+
+bot = Client(
+    "poolam",
+    bot_token = BOT_TOKEN,
+    api_id = API_ID,
+    api_hash = API_HASH
+)
+
+@bot.on_message(filters.audio)
+async def startt(bot, m):
+    await m.download('a.mp3')
+    song = AudioSegment.from_mp3("a.mp3")
+    end = AudioSegment.from_mp3("b.mp3")
+    with = song.append(end, crossfade=1500)
+    with.export('m.mp3')
+    await m.reply_audio(audio='m.mp3')
+
+
+bot.run()
 
 
 
 
 
-
-
-
-
-
-song = AudioSegment.from_wav("never_gonna_give_you_up.wav")
