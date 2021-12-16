1d503d1585c099012a95e3b538c6e3a42e351029
Soebb <87156166+Soebb@users.noreply.github.com>
2021-12-16 13:35:02 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,25 @@ import requests
 import urllib.parse
 
 
+BOT_TOKEN = " "
+API_ID = " "
+API_HASH = " "
+poolam_api_key = " "
+Price_rial = "100000"
+return_link = "http://cptid.ml/stplay/6TL4"
+
+
 bot = Client(
-    "Remove FwdTag",
-    bot_token = os.environ["BOT_TOKEN"],
-    api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"]
+    "poolam",
+    bot_token = BOT_TOKEN,
+    api_id = API_ID,
+    api_hash = API_HASH
 )
 
 @bot.on_message(filters.text)
 async def startt(bot, m):
     url = 'https://poolam.ir/invoice/request'
-    params = {"api_key":"dd82f80e2c0746c16a975884a73b73b5", "amount":20000, "return_url":urllib.parse.quote("http://tarafdari.com/node/2043656")}
+    params = {"api_key":poolam_api_key, "amount":int(Price_rial), "return_url":urllib.parse.quote(return_link)}
     r = requests.post(url, data=params)
     await m.reply(f"https://poolam.ir/invoice/pay/{r.json()['invoice_key']}")
 
