4aaa974b6303ad0198be9fe4542d16ef763971bc
Soebb <87156166+Soebb@users.noreply.github.com>
2021-12-17 12:12:46 +0330

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ bot = Client(
     api_hash = API_HASH
 )
 
-@bot.on_message(filters.audio)
+@bot.on_message(filters.private)
 async def startt(bot, m):
+    return await m.reply(m.text)
     await m.download(os.path.join(os.getcwd(), 'a.mp3'))
 
     song = AudioSegment.from_mp3(os.path.join(os.getcwd(), 'a.mp3'))
