ea21ebb716a5d6480078ab6dc2da52a58f82d8d2
Soebb <87156166+Soebb@users.noreply.github.com>
2021-12-20 22:17:12 +0330

    Update main.py

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ a2 = dir + '2.mp3'
 a3 = dir + '3.mp3'
 a6 = dir + '6.mp3'
 aac = dir + 'a.aac'
+org = dir + 'org.mp3'
 msgid = 0
 chatid = 0
 @bot.on_message(filters.text)
@@ -133,7 +134,14 @@ async def callback(bot, update):
                 a2_2 = AudioSegment.from_mp3(dir + '2.2.mp3')
                 aa2 = a2_1.append(a2_2)
                 aa2.export(dir+"2.mp3", format="mp3")
-                os.system(f'ffmpeg -i "{v}" -vn -i {a1} -vn -i {a2} -vn -i {a3} -vn -i {a6} -vn -filter_complex "[1]adelay=00000|00000[b]; [2]adelay={t2}|{t2}[c]; [3]adelay={t3_1}|{t3_1}[d]; [3]adelay={t3_2}|{t3_2}[e]; [3]adelay={t3_3}|{t3_3}[f]; [3]adelay={t3_4}|{t3_4}[g]; [3]adelay={t3_5}|{t3_5}[h]; [4]adelay={t6}|{t6}[i]; [0][b][c][d][e][f][g][h][i]amix=9" -c:a aac -b:a 125k -y {aac}')   
+                os.system(f'ffmpeg -i "{v}" -vn -y org.mp3')
+                aud2 = AudioSegment.from_mp3(a2)
+                aud3 = AudioSegment.from_mp3(a3)
+                audorg = AudioSegment.from_mp3(org)
+                aud1 = AudioSegment.from_mp3(a1)
+                aud6 = AudioSegment.from_mp3(a6)
+                
+                #os.system(f'ffmpeg -i "{v}" -vn -i {a1} -vn -i {a2} -vn -i {a3} -vn -i {a6} -vn -filter_complex "[1]adelay=00000|00000[b]; [2]adelay={t2}|{t2}[c]; [3]adelay={t3_1}|{t3_1}[d]; [3]adelay={t3_2}|{t3_2}[e]; [3]adelay={t3_3}|{t3_3}[f]; [3]adelay={t3_4}|{t3_4}[g]; [3]adelay={t3_5}|{t3_5}[h]; [4]adelay={t6}|{t6}[i]; [0][b][c][d][e][f][g][h][i]amix=9" -c:a aac -b:a 125k -y {aac}')   
                 time.sleep(10)
                 os.system(f'ffmpeg -i "{v}" -i {aac} -c copy -map 0:0 -map 1:0 -y "{vname}"')
                 processmsg.delete()
